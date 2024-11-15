2a400f4d576e19f2c689443d28dd4102caf4768d
smallgolem9515 <kbs454566@naver.com>
2024-11-15 15:21:00 +0900

    Squashed commit of the following:
    commit 0152289858baa74fde6f2e30c1745c3b4854140e
    Author: smallgolem9515 <kbs454566@naver.com>
    Date:   Fri Nov 15 15:20:26 2024 +0900
    
        [feat] 드래그로 가구 생성
    
        이미지를 드래그해서 플레인에 두면 생성 드래그시 마우스를 따라 이미지나 프리펩이 움직임

--- Assets/01_Scripts/Furniture/FurnitureSpec.cs
+++ Assets/01_Scripts/Furniture/FurnitureSpec.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FurnitureSpec", menuName = "Scriptable Objects/FurnitureSpec")]
 public class FurnitureSpec : ScriptableObject
 {
-    public int index; //가구 번호
-    public string name; //가구 이름
-    public string description; //가구 설명
-    public Sprite furnitureSprite; //가구 스프라이트
-    public GameObject furniturePrefeb; //가구 프리펩
+    [field:SerializeField] public int index{ get; private set; } //가구 번호
+    [field:SerializeField] new public string name { get; private set; } //가구 이름
+    [field:SerializeField] public string description { get; private set; } //가구 설명
+    [field:SerializeField] public Sprite furnitureSprite { get; private set; } //가구 스프라이트
+    [field:SerializeField] public GameObject furniturePrefeb { get; private set; } //가구 프리펩
 }
