0195d34a6fed8184c72f04495e494a2af2a62e2e
smallgolem9515 <kbs454566@naver.com>
2024-11-20 15:07:53 +0900

    [docs] 스크립터블 오브젝트 추가
    폰트 추가
    스프라이트 폴더 추가

--- Assets/01_Scripts/Furniture/UIManager.cs
+++ Assets/01_Scripts/Furniture/UIManager.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private FurnitureSpecRepository _repository;
     [SerializeField] private GameObject _slotPrefeb;
     [SerializeField] private GameObject _InputManager;
+    [SerializeField] private GridLayoutGroup _layoutGroup;
     private RectTransform _contentTransform;
     private Dictionary<int, FurnitureSpec> _slotDic = new Dictionary<int, FurnitureSpec>();
+    private float _slotDistance;
 
     private void Start()
     {
@@ -34,8 +37,9 @@ public class UIManager : MonoBehaviour
         }
         if (_slotDic.Count > 7) //슬롯이 스크롤 뷰보다 많아져서 안보여지면
         {
+            _slotDistance = _layoutGroup.cellSize.x + _layoutGroup.spacing.x;
             Vector2 size = _contentTransform.sizeDelta;
-            size.x -= (150*(7-_slotDic.Count)); //넘은 갯수만큼 늘려준다.
+            size.x -= (_slotDistance*(7-_slotDic.Count)); //넘은 갯수만큼 늘려준다.
             _contentTransform.sizeDelta = size;
         }
     }
