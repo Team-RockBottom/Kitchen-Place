23a555945bc331b588444907537e9492e3ad4c50
smallgolem9515 <kbs454566@naver.com>
2024-11-21 15:06:06 +0900

    [fix] 슬롯 사이즈 조정

--- Assets/01_Scripts/Furniture/UIManager.cs
+++ Assets/01_Scripts/Furniture/UIManager.cs
@@ -42,7 +42,7 @@ public class UIManager : MonoBehaviour
         Debug.Log(_layoutGroup.cellSize.y);
         Debug.Log(_layoutGroup.spacing.x);
         Vector2 size = _contentTransform.sizeDelta;
-        size.x -= (_slotDistance*(4-_slotDic.Count)); //넘은 갯수만큼 늘려준다.
+        size.x += (_slotDistance*_slotDic.Count); //넘은 갯수만큼 늘려준다.
         _contentTransform.sizeDelta = size;
     }
 }
